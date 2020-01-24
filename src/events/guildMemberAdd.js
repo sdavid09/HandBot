2dd451bcecad2d2cb295e1145055c19658e07f91
Shobin <shobin.david9@gmail.com>
2020-01-24 16:41:48 -0600

    fixed when users join server welcome notification

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -2,9 +2,9 @@
 const giphy= require ('../../src/extra/giphy');
 const { User } = require ('../user/base/user');
 
-module.exports = async (client, message) => {
+module.exports = async (client, member) => {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'general');
+    const channel = client.channels.find(ch => ch.name === 'general');
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
@@ -12,5 +12,6 @@ module.exports = async (client, message) => {
     let gif_url = await giphy.gifMessage("welcome" ,'pg-13');
     channel.send("", { files: [`${gif_url}`] })
     let user = await new User(member.id).get();
-    user.save()
+    user.setName(member.user.username);
+    user.save();
 }
